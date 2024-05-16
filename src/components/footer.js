e723e491155e97c8110b479f8636bc256135ad38
K-aigoro <aigorokazeem90@gmail.com>
2024-05-16 16:53:15 +0100

    update

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,17 +3,14 @@ import Order from "./order";
 // Footer App
 function Footer() {
   const Hour = new Date().getHours();
-  const openHour = 9;
+  const openHour = 8;
   const closeHour = 22;
   const isOpen = Hour >= openHour && Hour <= closeHour;
   console.log(isOpen);
   console.log(Hour);
-  // if (Hour >= openHour && Hour <= closeHour) alert("We are currently open!");
-  // else alert("Sorry we are closed");
 
   return (
     <footer className="footer">
-      {/* {new Date().toLocaleDateString()}. We are currently Open! */}
       {isOpen ? (
         <Order closeHour={closeHour} />
       ) : (
